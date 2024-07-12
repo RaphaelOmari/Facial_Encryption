2a894188e961d34246b197906f9fadfa173907ee
RaphaelOmari <127858530+RaphaelOmari@users.noreply.github.com>
2024-07-12 00:26:18 +0000

    first_commit

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
 #start
+import threading
+import cv2
+from deepface import deepface
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_WEIGHT, 480)
+counter = 0
+
+reference_img = cv2.imread("reference.jpg")
+
+face_match = False
+
+def check_face(frame):
+    global face_match
+    try:
+        if DeepFace.verify(frame, reference_img.copy())["verified"]:
+            face_match = True
+        else:
+            face_match = False
+    except ValueError as e:
+        face_match = False
+        print(e)
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        if counter%30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(), )).start()
+            except ValueError:
+                pass
+        counter += 1
+        if face_match:
+            cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+
+        cv2.imshow("video", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
