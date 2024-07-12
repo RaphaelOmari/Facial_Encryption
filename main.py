e8af887634e4e96d5d0974506baf3c2ddfc332bc
RaphaelOmari <127858530+RaphaelOmari@users.noreply.github.com>
2024-07-12 01:04:03 +0000

    second_push

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
-#start
 import threading
 import cv2
-from deepface import deepface
+from deepface import DeepFace
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_WEIGHT, 480)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 counter = 0
 
 reference_img = cv2.imread("reference.jpg")
 
 face_match = False
+lock = threading.Lock()
 
 def check_face(frame):
     global face_match
     try:
-        if DeepFace.verify(frame, reference_img.copy())["verified"]:
-            face_match = True
-        else:
+        result = DeepFace.verify(frame, reference_img.copy())["verified"]
+        with lock:
+            face_match = result
+    except Exception as e:
+        with lock:
             face_match = False
-    except ValueError as e:
-        face_match = False
         print(e)
+
 while True:
     ret, frame = cap.read()
 
     if ret:
-        if counter%30 == 0:
+        if counter % 30 == 0:
             try:
-                threading.Thread(target=check_face, args=(frame.copy(), )).start()
-            except ValueError:
-                pass
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except Exception as e:
+                print(e)
+
         counter += 1
-        if face_match:
-            cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-        else:
-            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+
+        with lock:
+            if face_match:
+                cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+            else:
+                cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
 
         cv2.imshow("video", frame)
 
@@ -44,4 +47,5 @@ while True:
     if key == ord('q'):
         break
 
+cap.release()
 cv2.destroyAllWindows()
